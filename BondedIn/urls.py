f2474980520ca7aafb12320069a48eab2413e151
Lucas <lucas@laragno-MOV.(none)>
2013-10-08 15:12:08 -0300

    uploading content

---
+++ BondedIn/urls.py
@@ -0,0 +1,24 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'BondedIn.views.home', name='home'),
+    # url(r'^BondedIn/', include('BondedIn.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+
+   
+
+    url(r'^login/?$', 'linkedinapp.views.oauth_login'),
+    url(r'^logout/?$', 'linkedinapp.views.oauth_logout'),
+    url(r'^login/authenticated/?$', 'linkedinapp.views.oauth_authenticated'),
+    url(r'^$','linkedinapp.views.home'),
+)
