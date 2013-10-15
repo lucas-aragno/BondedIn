f6f4b0dcb8e9a05f5e3088db53f2727b0100f327
ccenoz <ccenoz@devspark.com>
2013-10-15 16:08:34 -0300

    test initial commit

--- linkedinapp/tests.py
+++ linkedinapp/tests.py
@@ -6,11 +6,46 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+def setUp(self):
+        self.tech = ('python','java')
+		
+class TechTestCase(TestCase):
+        
+    def test_technology_is_present(self):
+        """Technologies which are present"""
+		for i in self.tech:
+
+        # Get technology in list
+				response = self.client.get('/list/'+list[i])
+
+        # Check that the response is 200 OK.
+				if self.assertEqual(response.status_code, 200):
+					pass
+				else:
+				# Is not OK
+					print (list[i]+'is not present')
+					
+class ProvinceTestCase(TestCase):
+    def setUp(self):
+        self.prov = ('misiones','formosa','chaco','buenos-aires','salta','tucuman')
+        
+    def test_province_is_present(self):
+        """Technologies which are present"""
+		for i in self.tech:
+			for j in self.prov: 
+
+        # Get technology and province in lists
+				response = self.client.get('/list/'+list[i]+list[j])
+
+        # Check that the response is 200 OK.
+				if self.assertEqual(response.status_code, 200):
+					pass
+				else:
+				# Is not OK
+					print (list[i]+list[j]'is not present')
+		 
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+	
+
+       
