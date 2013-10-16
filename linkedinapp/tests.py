355e61a6ea9a7c5cf58b08f79faa3c4ad42dc127
ccenoz <ccenoz@devspark.com>
2013-10-16 17:24:44 -0300

    modifying

--- linkedinapp/tests.py
+++ linkedinapp/tests.py
@@ -1,24 +1,19 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
 
 from django.test import TestCase
+
 def setUp(self):
         self.tech = ('python','java')
 		
 class TechTestCase(TestCase):
         
     def test_technology_is_present(self):
-        """Technologies which are present"""
+        
 		for i in self.tech:
 
-        # Get technology in list
+				# Get technology in list
 				response = self.client.get('/list/'+list[i])
 
-        # Check that the response is 200 OK.
+				# Check that the response is 200 OK.
 				if self.assertEqual(response.status_code, 200):
 					pass
 				else:
@@ -30,19 +25,19 @@ class ProvinceTestCase(TestCase):
         self.prov = ('misiones','formosa','chaco','buenos-aires','salta','tucuman')
         
     def test_province_is_present(self):
-        """Technologies which are present"""
+   
 		for i in self.tech:
 			for j in self.prov: 
 
-        # Get technology and province in lists
+				# Get technology and province in lists
 				response = self.client.get('/list/'+list[i]+list[j])
 
-        # Check that the response is 200 OK.
+				# Check that the response is 200 OK.
 				if self.assertEqual(response.status_code, 200):
 					pass
 				else:
 				# Is not OK
-					print (list[i]+list[j]'is not present')
+					print (list[j]+'is not present')
 		 
 
 
