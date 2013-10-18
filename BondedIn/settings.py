f9a1206f420d71fa4b43690374ce884fb66dbd9c
Lucas <lucas@laragno-MOV.(none)>
2013-10-18 11:52:15 -0300

    fixed infinite loop

--- BondedIn/settings.py
+++ BondedIn/settings.py
@@ -24,7 +24,7 @@ DATABASES = {
         'NAME': 'Geo',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'fany',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -81,7 +81,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/polak/workspace/pythonWorkspace/BondedIn/static/',
+    '/home/lucas/BondedInrepo/static/',
 )
 
 # List of finder classes that know how to find static files in
