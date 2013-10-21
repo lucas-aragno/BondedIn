ff840755e01f34488b9351158862459a516c3197
Martín Pascal <mspascal@gmail.com>
2013-10-21 16:26:37 -0400

    Fields added to Company

--- Cache/Company.py
+++ Cache/Company.py
@@ -1,1 +1,129 @@
 # Clase que representa la compania
+Class Company:
+    id
+    name
+    universal-name
+    email-domains
+    company-type
+    ticker
+    website-url
+    industries
+    status
+    logo-url
+    square-logo-url
+    blog-rss-url
+    twitter-id
+    employee-count-range
+    specialties
+    locations
+    description
+    end-year
+    num-followers
+
+    def getId(self):
+        return self.id
+
+    def setId(self, id):
+        self.id = id
+
+    def getName(self):
+        return self.name
+
+    def setName(self, name):
+        return self.name
+
+    def getEmailDomains(self):
+        return self.email-domains
+
+    def setEmailDomains(self, email-domains):
+        self.email.domains = email-domains
+
+    def getCompanyType(self):
+        return self.company-type
+
+    def setCompanyType(self, company-type):
+        self.company-type = company-type
+
+    def getTiker(self):
+        return self.tiker
+
+    def setTiker(self, tiker):
+        self.tiker = tiker
+
+    def getWebsiteUrl(self):
+        return self.website-url
+
+    def setWebsiteUrl(self, website-url):
+        self.website-url = website-url
+
+    def getIndustries(self):
+        return self.industries
+
+    def setIndustries(self, industries):
+        self.industries = industries
+
+    def getStatus(self):
+        return self.status
+
+    def setStatus(self, status):
+        self.status = status
+
+    def getLogoUrl(self):
+        return self.logo-url
+
+    def setLogoUrl(self, logo-url):
+        self.logo-url = logo-url
+
+    def getSquareLogoUrl(self):
+        return self.square-logo-url
+
+    def setSquareLogoUrl(self, square-logo-url):
+        self.square-logo-url = square-logo-url
+
+    def getBlogRssUrl(self):
+        return self.blog-rss-url
+
+    def setBlogRssUrl(self, blog-rss-url):
+        self.blog-rss-url = blog-rss-url
+
+    def getTwitterId(self):
+        return self-twitter-id
+
+    def setTwitterId(self, twitter-id):
+        self.twitter-id = twitter-id
+
+    def getEmployeeCountRange(self):
+        return self.employee-count-range
+
+    def setEmployeeCountRange(self, employee-count-range):
+        self.employee-count-range = employee-count-range
+
+    def getSpecialities(self):
+        return self.specialities
+
+    def setSpecialities(self, specialities):
+        self.specialities = specialities
+
+    def getLocations(self):
+        return self.locations
+
+    def setLocations(self, locations):
+        self.locations = locations
+
+    def getDescription(self):
+        return self.descriptions
+
+    def setDescription(self, description):
+        self.description = description
+
+    def getEndYear(self):
+        return self.end-year
+
+    def setEndYear(self, end-year):
+        self.end-year = end-year
+
+    def getNumFollowers(self):
+        return self.num-followers
+
+    def setNumFollowers(self, num-followers):
+        self.num-followers = num-followers
