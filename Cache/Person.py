f4507e6fc8376aeb16566f87e0ed819712e4c9ae
fvega <fvega@devspark.com>
2013-10-21 17:56:40 -0300

    Creacion de la clase Person la cual contiene los datos personales de la persona

---
+++ Cache/Person.py
@@ -0,0 +1,101 @@
+class Person:
+	id
+	first-name
+	last-name
+	location-name
+	location-code 
+	email-address
+	skills
+	educations
+	date-of-birth
+	phone-numbers
+	main-address
+	primary-twitter-account
+	picture-url
+	public-profile-url
+
+	
+	def getId(self):
+		return id
+		
+	def setId(self,id):
+		self.id= id
+		
+	
+	def getFirstName(self):
+		return first-name
+	
+	def setFirstName(self,firstName):
+		self.first-name= firstName
+		
+	def getLastName(self):
+		return last-name
+	
+	def setLastName(self, lastName):
+		self.last-name= lastName
+		
+	def getLocationName(self):
+		return location-name
+	
+	def setLocationName(self, locationName):
+		self.location-name = locationName
+		
+	def getLocationCode(self):
+		return location-code
+	
+	def setLocationCode(self, locationCode):
+		this.location-code=locationCode
+		
+	def getEmailAddress(self):
+		return return email-address
+	
+	def sertEmailAddress(self,emailAddress)
+		this.email-address=emailAddress
+		
+	def getSkills(self):
+		return skills
+	
+	def setSkills(self,skill)
+		this.skills =skill
+		
+	def getEducations(self):
+		return educations
+	
+	def setEducation(self, education):
+		this.education
+		
+	def getDateOfBirth(self):
+		return date-of-birth
+	
+	def setDateOdBirth(self, date):
+		this.date-of-birth= date
+		
+	def getPhoneNumber(self):
+		return phone-numbers
+	
+	def setPhoneNumbers(self, phone):
+		this.phone-numbers=phone
+		
+	def getMainAdress(self):
+		return main-address
+	
+	def setMainAddress(self, address):
+		this.main-address=address
+		
+	def getTwitterAccount(self):
+		return primary-twitter-account
+		
+	def setTwitterAccount(self, account):	
+		this.primary-twitter-account=account
+		
+	def getPictureUrl(self):
+		return picture-url
+	
+	def setPictureUrl(self, picture):
+		this.picture-url=picture
+		
+	def getPublicProfileUrl(self):
+		return public-profile-url
+	
+	def setPublicProfileUrl(self, urlProfile):
+		this.public-profile-url=urlProfile
